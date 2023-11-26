9af215f1186a1b369548f1623197e4cb2a217c0f
LANSGANBS <2140967451@qq.com>
2023-11-26 10:06:52 +0800

    main

--- P8779 [蓝桥杯 2022 省 A] 推导部分和.cpp
+++ P8779 [蓝桥杯 2022 省 A] 推导部分和.cpp
@@ -10,10 +10,58 @@ using namespace std;
 #define ll long long
 #define int ll
 
+const int maxn = 200000 + 10;
+int n, m, a, b, s;
+int par[maxn], val[maxn];
+
+void init(int l, int r)
+{
+    for (int i = l; i <= r; i++)
+        par[i] = i, val[i] = 0;
+}
+
+int find(int x)
+{
+    if (par[x] == x)
+        return x;
+    else
+    {
+        int root = find(par[x]);
+        val[x] += val[par[x]];
+        return par[x] = root;
+    }
+}
+
 void solve()
 {
     // wout <<L" "<<endl;
     // cout << R"( )" << endl;
+    cin >> n >> m;
+    int q;
+    cin >> q;
+    init(0, n);
+    int ans = 0;
+    for (int i = 1, a, b, s; i <= m; i++)
+    {
+        cin >> a >> b >> s;
+        a--;
+        int t1 = find(a), t2 = find(b);
+        if (t1 != t2)
+        {
+            par[t2] = t1;
+            val[t2] = -val[b] + s + val[a];
+        }
+    }
+    while (q--)
+    {
+        cin >> a >> b;
+        a--;
+        int t1 = find(a), t2 = find(b);
+        if (t1 != t2)
+            cout << "UNKNOWN" << endl;
+        else
+            cout << val[b] - val[a] << endl;
+    }
 }
 
 signed main()
